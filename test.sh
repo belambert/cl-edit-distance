5f41fc248092d34e8599077ceebdbf49be228332
Ben Lambert <ben@benjaminlambert.com>
2017-03-19 19:00:54 -0400

    Attempted fixes to test.sh script.

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-set -ev
 
 if [ "${COVERALLS}" = "true" ]; then
+    echo "Running test with coverage..."
     cl -l edit-distance-test -l edit-distance -l cl-coveralls
-       -e '(coveralls:with-coveralls (:exclude (list "t"))
-             (unless (lisp-unit:run-tests :all :edit-distance-tests)
-               (uiop:quit 1)))'
+       -e "(coveralls:with-coveralls (:exclude (list \"t\")) \
+             (unless (lisp-unit:run-tests :all :edit-distance-tests) \
+               (uiop:quit 1)))"
 else
-
+    echo "Running test without coverage..."
     cl -l edit-distance-test -l edit-distance
-       -e '(unless (lisp-unit:run-tests :all :edit-distance-tests)
-             (uiop:quit 1))'
+       -e "(unless (lisp-unit:run-tests :all :edit-distance-tests) \
+             (uiop:quit 1))"
 fi
