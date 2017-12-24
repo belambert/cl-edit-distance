fbfbcd45fd0b86a41863d1af00d3a1c0460ba2b2
Ben Lambert <ben@benjaminlambert.com>
2017-12-23 21:21:14 -0500

    Add short copyright notice to distance.lisp source file.

--- src/distance.lisp
+++ src/distance.lisp
@@ -1,5 +1,8 @@
-;;; Author: Ben Lambert
-;;; ben@benjaminlambert.com
+;; cl-editdistance is licensed under a
+;; Creative Commons Attribution 4.0 International License.
+
+;; You should have received a copy of the license along with this
+;; work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
 
 (in-package :edit-distance)
 
