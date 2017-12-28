051baf6c997ebec0091be192ee01d99b0a1ec1fd
Ben Lambert <ben@benjaminlambert.com>
2017-12-28 14:10:52 -0500

    Fix test coverage reporting in Travis CI.

--- src/test.lisp
+++ src/test.lisp
@@ -17,34 +17,31 @@
 (in-package :edit-distance-tests)
 
 (define-test test-distance-fast
-    (let ((result (compute-edit-distance '(1 2 3) '(1 2 4))))
-      (assert-equal (distance-errors result) 1)
-      (assert-equal (distance-matches result) 2)))
+    (let ((result (distance '(1 2 3) '(1 2 4))))
+      (assert-equal 1 result)))
 
 (define-test test-distance-slow
   (multiple-value-bind (path distance)
-      (levenshtein-distance '("1" "2" "3") '("1" "2" "4") :return-path t)
+      (diff '("1" "2" "3") '("1" "2" "4"))
     (assert-equal path '((:MATCH "1" "1") (:MATCH "2" "2") (:SUBSTITUTION "3" "4")))
     (assert-equal distance 1)))
 
 (define-test test-printing
   (multiple-value-bind (path distance)
-      (levenshtein-distance '(0 1 2 3) '(1 2 4 5) :return-path t)
+      (diff '(0 1 2 3) '(1 2 4 5))
     (assert-equal distance 3)
-    (print-differences path)))
+    (format-diff path)))
 
 (define-test test-arrays
-    (let ((result (compute-edit-distance #(1 2 3) #(1 2 4))))
-      (assert-equal (distance-errors result) 1)
-      (assert-equal (distance-matches result) 2)))
+    (let ((result (distance #(1 2 3) #(1 2 4))))
+      (assert-equal 1 result)))
 
 (define-test test-strings
-    (let ((result (compute-edit-distance "123" "124")))
-      (assert-equal (distance-errors result) 1)
-      (assert-equal (distance-matches result) 2)))
+    (let ((result (distance "123" "124")))
+      (assert-equal 1 result)))
 
 (define-test test-string-printing
   (multiple-value-bind (path distance)
-      (levenshtein-distance "0123" "1245" :return-path t)
+      (diff "0123" "1245")
     (assert-equal distance 3)
-    (print-differences path)))
+    (format-diff path)))
